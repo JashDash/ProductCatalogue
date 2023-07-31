d9946f8819e2be4176e00c6c6a6603f26495313e
ash-coder01 <ashleshadhanjal@gmail.com>
2023-07-31 15:49:40 +0530

    Remove unused files

--- product-catalogue/src/App.tsx
+++ product-catalogue/src/App.tsx
@@ -1,23 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 import SearchScreen from './screens/SearchScreen';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <SearchScreen />
-              <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
