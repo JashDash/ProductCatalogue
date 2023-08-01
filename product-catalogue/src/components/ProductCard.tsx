780a4c2d2a048b8cf27045121f1a3237e32fdfe0
Ashlesha <ashleshadhanjal@gmail.com>
2023-08-01 14:13:59 +0530

    Create suggestion product card UI

--- product-catalogue/src/components/ProductCard.tsx
+++ product-catalogue/src/components/ProductCard.tsx
@@ -4,6 +4,19 @@ import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import { LatestTrendsItemCard } from './LatestTrendsItemCard';
+import { faker } from '@faker-js/faker';
+  
+const createCars = (num = 5) => {
+    let cars = new Array();
+    for (let i = 0; i < num; i++) {
+        cars.push(faker.vehicle.manufacturer());
+    }
+    return cars;
+};
+
+let fakeCarsSuggestions = createCars(5);
+let fakeCarsTrending = createCars(5);
 
 export function ProductCard() {
     return (
@@ -11,15 +24,25 @@ export function ProductCard() {
             <Card sx={{ width: 700,
                 position: "absolute", 
                 minWidth: 275,
-                transform: 'translate(-50%, -50%)',
-                marginTop:'6%', marginLeft:'50%' }}>
+                transform: 'translate(-50%, 0%)',
+                marginTop:'80px', marginLeft:'50%', pl:2, pr:2, pt:1, pb:1, borderRadius: 1 }}>
             <CardContent>
-                <Typography sx={{ fontSize: 14 }} color="black" gutterBottom>
+                <Typography variant="subtitle1" sx={{ fontWeight: 'bold' }} color="black" gutterBottom>
                 Latest Trends
                 </Typography>
-                <Typography sx={{ fontSize: 14 }} color="black" gutterBottom>
+                <div style={{display:'flex', justifyContent: 'space-between', marginTop: 10}}>
+                    {fakeCarsTrending.map((fakeCar,index)=>{
+                        return <LatestTrendsItemCard manufacturer = {fakeCarsTrending[index]}/> 
+                    })}
+                </div>
+                <Typography variant="subtitle1" sx={{ marginTop: 2, fontWeight: 'bold' }} color="black" gutterBottom>
                 Popular Suggestions
                 </Typography>
+                <div style={{display:'flex', flexDirection:'column' , justifyContent: 'space-between', marginTop: 10}}>
+                    {fakeCarsTrending.map((fakeCar,index)=>{
+                        return <Typography variant="caption" color="black" gutterBottom>{fakeCarsSuggestions[index]}</Typography> 
+                    })}
+                </div>
             </CardContent>
             </Card>
         </div>
