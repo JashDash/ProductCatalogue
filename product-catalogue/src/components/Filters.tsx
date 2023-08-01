250e85f6342b484f7c2e9fc5c2ca8ba2d551f702
Jashan Devgan <jashandevgan@gmail.com>
2023-08-02 00:51:04 +0530

    Add brand names to filter

--- product-catalogue/src/components/Filters.tsx
+++ product-catalogue/src/components/Filters.tsx
@@ -9,6 +9,8 @@ import FormGroup from '@mui/material/FormGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
 import Divider from '@mui/material/Divider';
+import { manufacturerArray } from "../data/DisplayCarsArray";
+
 
 export function Filters() {
     const [openBrandFilter, setOpenBrandFilter] = React.useState(true);
@@ -27,51 +29,52 @@ export function Filters() {
     return (
         <div>
             <List
-            sx={{width: '230px', bgcolor: 'background.paper', p: 2 }}
-            component="nav"
-            aria-labelledby="nested-list-subheader"
+                sx={{ width: '230px', bgcolor: 'background.paper', p: 2 }}
+                component="nav"
+                aria-labelledby="nested-list-subheader"
             >
-            <ListItemButton onClick={handleClickBrandFilter}>
-                <ListItemText primary="BRAND" />
-                {openBrandFilter ? <ExpandLess /> : <ExpandMore />}
-            </ListItemButton>
-            <Collapse in={openBrandFilter} timeout="auto" unmountOnExit>
-                <List component="div" disablePadding>
-                    <FormGroup sx={{pl: 4}}>
-                        <FormControlLabel control={<Checkbox />} label="Mango" />
-                        <FormControlLabel control={<Checkbox />} label="H&M" />
-                    </FormGroup>
-                </List>
-            </Collapse>
-            <Divider style={{marginTop: '10px'}} />
-            <ListItemButton onClick={handleClickPriceFilter}>
-                <ListItemText primary="PRICE RANGE" />
-                {openPriceFilter ? <ExpandLess /> : <ExpandMore />}
-            </ListItemButton>
-            <Collapse in={openPriceFilter} timeout="auto" unmountOnExit>
-                <List component="div" disablePadding>
-                    <FormGroup sx={{pl: 4}}>
-                        <FormControlLabel control={<Checkbox />} label="Under 500" />
-                        <FormControlLabel control={<Checkbox />} label="1000 to 3000" />
-                    </FormGroup>
-                </List>
-            </Collapse>
-            <Divider style={{marginTop: '10px'}}/>
-            <ListItemButton onClick={handleClickStarsFilter}>
-                <ListItemText primary="RATING" />
-                {openStarsFilter ? <ExpandLess /> : <ExpandMore />}
-            </ListItemButton>
-            <Collapse in={openStarsFilter} timeout="auto" unmountOnExit>
-                <List component="div" disablePadding>
-                    <FormGroup sx={{pl: 4}}>
-                        <FormControlLabel control={<Checkbox />} label="5 star" />
-                        <FormControlLabel control={<Checkbox />} label="4 star" />
-                        <FormControlLabel control={<Checkbox />} label="3 star" />
-                        <FormControlLabel control={<Checkbox />} label="2 star" />
-                        <FormControlLabel control={<Checkbox />} label="1 star" />
-                    </FormGroup>
-                </List>
-            </Collapse>
+                <ListItemButton onClick={handleClickBrandFilter}>
+                    <ListItemText primary="BRAND" />
+                    {openBrandFilter ? <ExpandLess /> : <ExpandMore />}
+                </ListItemButton>
+                <Collapse in={openBrandFilter} timeout="auto" unmountOnExit>
+                    <List component="div" disablePadding>
+                        <FormGroup sx={{ pl: 4 }}>
+                            {manufacturerArray.map((val: any) => <FormControlLabel control={<Checkbox />} label={val} />)}
+                            <FormControlLabel control={<Checkbox />} label="Mango" />
+                            <FormControlLabel control={<Checkbox />} label="H&M" />
+                        </FormGroup>
+                    </List>
+                </Collapse>
+                <Divider style={{ marginTop: '10px' }} />
+                <ListItemButton onClick={handleClickPriceFilter}>
+                    <ListItemText primary="PRICE RANGE" />
+                    {openPriceFilter ? <ExpandLess /> : <ExpandMore />}
+                </ListItemButton>
+                <Collapse in={openPriceFilter} timeout="auto" unmountOnExit>
+                    <List component="div" disablePadding>
+                        <FormGroup sx={{ pl: 4 }}>
+                            <FormControlLabel control={<Checkbox />} label="Under 500" />
+                            <FormControlLabel control={<Checkbox />} label="1000 to 3000" />
+                        </FormGroup>
+                    </List>
+                </Collapse>
+                <Divider style={{ marginTop: '10px' }} />
+                <ListItemButton onClick={handleClickStarsFilter}>
+                    <ListItemText primary="RATING" />
+                    {openStarsFilter ? <ExpandLess /> : <ExpandMore />}
+                </ListItemButton>
+                <Collapse in={openStarsFilter} timeout="auto" unmountOnExit>
+                    <List component="div" disablePadding>
+                        <FormGroup sx={{ pl: 4 }}>
+                            <FormControlLabel control={<Checkbox />} label="5 star" />
+                            <FormControlLabel control={<Checkbox />} label="4 star" />
+                            <FormControlLabel control={<Checkbox />} label="3 star" />
+                            <FormControlLabel control={<Checkbox />} label="2 star" />
+                            <FormControlLabel control={<Checkbox />} label="1 star" />
+                        </FormGroup>
+                    </List>
+                </Collapse>
             </List>
         </div>
     )
--- product-catalogue/src/data/DisplayCarsArray.tsx
+++ product-catalogue/src/data/DisplayCarsArray.tsx
@@ -1,5 +1,6 @@
 import { faker } from '@faker-js/faker';
 
+let manufacturerArray: any = [];
 const createCars = (num = 100) => {
     let cars = new Array();
     for (let i = 0; i < num; i++) {
@@ -18,11 +19,15 @@ let fakeCarsTrendingDisplay = fakeCarsTrending.slice(0, 5);
 
 
 const createItem = () => {
-return {
-    manufacturer: faker.vehicle.manufacturer(),
-    price: Math.floor(Math.random() * 100 + 1),
-    rating: Math.floor(Math.random() * 5) + 1,
-    numrating:  Math.floor(Math.random() * (1000 - 100) ) + 100,
+    const brand = faker.vehicle.manufacturer();
+    if (manufacturerArray.indexOf(brand) === -1) {
+        manufacturerArray.push(brand);
+    }
+    return {
+        manufacturer: brand,
+        price: Math.floor(Math.random() * 100 + 1),
+        rating: Math.floor(Math.random() * 5) + 1,
+        numrating: Math.floor(Math.random() * (1000 - 100)) + 100,
     };
 };
 
@@ -36,5 +41,6 @@ const createCarItems = (num = 100) => {
 
 let fakeCarItems = createCarItems(100);
 let fakeCarItemsDisplay = fakeCarItems.slice(0, 10);
+manufacturerArray = manufacturerArray.slice(0, 10);
 
-export {fakeCarsSuggestions, fakeCarsTrending, fakeCarsSuggestionsDisplay, fakeCarsTrendingDisplay, fakeCarItems, fakeCarItemsDisplay}
+export { fakeCarsSuggestions, fakeCarsTrending, fakeCarsSuggestionsDisplay, fakeCarsTrendingDisplay, fakeCarItems, fakeCarItemsDisplay, manufacturerArray }
